623e1dee3f670a1856f068c024d1c562ddeeeea9
Fredrik Wahlqvist <fwahlqvist@gmail.com>
2012-10-02 22:49:15 +0100

    Intial set up of module.

---
+++ src/Album/Controller/AlbumController.php
@@ -0,0 +1,31 @@
+<?php
+// module/Album/src/A;bum/Controller/AlbumController.php
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+    
+    public function indexAction()
+    {
+        
+    }
+    
+    public function addAction()
+    {
+        
+    }
+    
+    public function editAction()
+    {
+        
+    }
+    
+    public function deleteActions()
+    {
+        
+    }
+    
+}
